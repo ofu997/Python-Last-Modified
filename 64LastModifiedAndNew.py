44a5562db9c7d59939250cbb92a41f03bb49a877
oliver fu <ofu997@gmail.com>
2017-07-18 15:57:59 -0700

    delete comments

--- 64LastModifiedAndNew.py
+++ 64LastModifiedAndNew.py
@@ -22,11 +22,6 @@ src="C://Users//Oliver//Desktop//folder_A//"
 dst="C://Users//Oliver//Desktop//folder_B//"
 
 myfiles = os.listdir(src)
-#print(myfiles)
-#print("age of python file: ")
-#print(datetime.datetime.fromtimestamp(os.path.getmtime("python63filemoving.py")))
-#print("age of txt file: ")
-#print(datetime.datetime.fromtimestamp(os.path.getmtime(src+"1.txt")))
 
 def Copier(src,dst):
   now = datetime.datetime.now()
@@ -44,89 +39,6 @@ def Copier(src,dst):
 Copier(src,dst)
 
 
-'''
-#research
-#https://stackoverflow.com/questions/12131766/python-date-time-comparison-using-timestamps-timedelta
-
-# SAVE THIS
-now = datetime.datetime.now()
-print(now)
-filename=input("write name of file")
-#osFunction=os.path.getmtime(filename)
-then = datetime.datetime.fromtimestamp(os.path.getmtime(filename))
-tdelta = now - then
-print(tdelta)
-seconds = tdelta.total_seconds()
-# can't get total_hours, why?
-print(seconds)
-
-if seconds > (60*60*24):
-  print("old")
-else:
-  print("newer")
-'''
-'''
-
-'''
-'''
-now = datetime.datetime.now()
-print(now)
-filename=input("write name of python file: ")
-
-## if recently created, it must have been
-## recently modified as well, so using getmtime()
-
-osFunction=os.path.getmtime(filename)
-then = datetime.datetime.fromtimestamp(osFunction)
-tdelta = now - then
-print(tdelta)
-seconds = tdelta.total_seconds()
-# can't get total_hours, why?
-print(seconds)
-
-if seconds > (60*60*24):
-  print("old")
-else:
-  print("newer")
-
-'''
-
-'''
-f = os.path.getctime("64LastModifiedAndNew.py")
-f2=datetime.datetime.fromtimestamp(f)
-print(f2)
-print(type(f2))
-# disc f3=datetime.strptime(f2, "%Y-%m-%d")
-'''
-'''
-print(f3)
-print(type(f3))
-'''
-'''
-
-g = os.path.getctime("vampire.py")
-g2=datetime.datetime.fromtimestamp(g)
-print(g2)
-print(type(g2))
-
-now=datetime.datetime.today()
-print(now)
-print(type(now))
-
-d1=now-g2
-print("d1: ")
-print(d1)
-print(type(d1))
-
-if d1.total_hours > timedelta(hours=24):
-  print("old")
-else:
-  print("new")
-  
-# dif1=d1.total_hours()
-
-# print(d1)
-'''
 
 
 
